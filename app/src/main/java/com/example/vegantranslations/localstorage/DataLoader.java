6ba0470254dd4ffcea1939c833cd13dd265e7c93
Marina Ionel <marinicaionel@gmail.com>
2020-04-04 19:58:43 +0200

    Update DataLoader.java

--- app/src/main/java/com/example/vegantranslations/localstorage/DataLoader.java
+++ app/src/main/java/com/example/vegantranslations/localstorage/DataLoader.java
@@ -6,6 +6,7 @@ import androidx.annotation.NonNull;
 
 import com.example.vegantranslations.model.Category;
 import com.example.vegantranslations.model.NonVeganProduct;
+import com.example.vegantranslations.model.Purpose;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.firestore.FirebaseFirestore;
@@ -17,11 +18,13 @@ import java.util.Map;
 
 import static com.example.vegantranslations.repository.Collections.CATEGORY;
 import static com.example.vegantranslations.repository.Collections.NON_VEGAN_PRODUCTS;
+import static com.example.vegantranslations.repository.Collections.PURPOSE;
 
 public class DataLoader {
     private FirebaseFirestore firestore = FirebaseFirestore.getInstance();
     private Map<String, Category> categoryMap = new HashMap<>();
     private Map<String, NonVeganProduct> nonVeganProductMap = new HashMap<>();
+    private Map<String, Purpose> purposeMap = new HashMap<>();
     private final String TAG = "Data Loader";
 
     public void populateLocalDatabaseFromFirebase() {
@@ -37,12 +40,15 @@ public class DataLoader {
                                 Category tmp = new Category(id, (String) document.get("name"));
                                 categoryMap.put(id, tmp);
                             }
+                            loadProducts();
                         } else {
                             Log.d(TAG, "Error getting documents: ", task.getException());
                         }
                     }
                 });
+    }
 
+    private void loadProducts() {
         firestore.collection(NON_VEGAN_PRODUCTS).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
             @Override
             public void onComplete(@NonNull Task<QuerySnapshot> task) {
@@ -52,6 +58,25 @@ public class DataLoader {
                         String id = document.getId();
                         NonVeganProduct tmp = new NonVeganProduct((String) document.get("name"), document.getId(), (String) document.get("category_id"));
                         nonVeganProductMap.put(id, tmp);
+                    }
+                    loadPurposes();
+                } else {
+                    Log.d(TAG, "Error getting documents: ", task.getException());
+                }
+            }
+        });
+    }
+
+    private void loadPurposes() {
+        firestore.collection(PURPOSE).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());
+                        String id = document.getId();
+                        Purpose tmp = new Purpose(document.getId(), (String) document.get("name"));
+                        purposeMap.put(id, tmp);
                     }
                 } else {
                     Log.d(TAG, "Error getting documents: ", task.getException());
