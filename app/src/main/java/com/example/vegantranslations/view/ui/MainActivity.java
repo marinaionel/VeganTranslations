a4d0eae81fd2b8fca9ae58355eb63954d28e0350
Marina Ionel <marinicaionel@gmail.com>
2020-04-19 10:09:17 +0200

    Update

--- app/src/main/java/com/example/vegantranslations/view/ui/MainActivity.java
+++ app/src/main/java/com/example/vegantranslations/view/ui/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.view.Window;
+import android.view.animation.Animation;
 import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -69,12 +70,36 @@ public class MainActivity extends AppCompatActivity {
         makeItVegan.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                makeItVegan.getAnimation().setAnimationListener(new Animation.AnimationListener() {
+                    @Override
+                    public void onAnimationStart(Animation animation) {
+
+                    }
+
+                    @Override
+                    public void onAnimationEnd(Animation animation) {
+                        Intent myIntent = new Intent(MainActivity.this, ShowResultsActivity.class);
+                        MainActivity.this.startActivityForResult(myIntent, 1);
+                    }
+
+                    @Override
+                    public void onAnimationRepeat(Animation animation) {
+
+                    }
+                });
+
                 makeItVegan.doResult(true);
-                Intent myIntent = new Intent(MainActivity.this, ShowResultsActivity.class);
-//                myIntent.putExtra("key", value);
-                MainActivity.this.startActivity(myIntent);
             }
         });
+    }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1) {
+            if (resultCode == RESULT_OK) {
+                makeItVegan.reset();
+            }
+        }
     }
 }
--- app/src/main/java/com/example/vegantranslations/view/ui/ShowResultsActivity.java
+++ app/src/main/java/com/example/vegantranslations/view/ui/ShowResultsActivity.java
@@ -3,6 +3,7 @@ package com.example.vegantranslations.view.ui;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.lifecycle.ViewModelProvider;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 
@@ -14,12 +15,19 @@ public class ShowResultsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_show_results);
-
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getSupportActionBar().hide();
 
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_results);
+
         showResultsViewModel = new ViewModelProvider(this).get(ShowResultsViewModel.class);
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        setResult(RESULT_OK, intent);
+        finish();
     }
 }
