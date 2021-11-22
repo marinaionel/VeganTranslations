ad1bcc4d8148ab951fa387749079886a7e6b95f0
Marina Ionel <marinicaionel@gmail.com>
2021-11-22 16:39:17 +0100

    Update ApiConstants.java

--- app/src/main/java/com/example/vegantranslations/model/network/ApiConstants.java
+++ app/src/main/java/com/example/vegantranslations/model/network/ApiConstants.java
@@ -3,5 +3,4 @@ package com.example.vegantranslations.model.network;
 public class ApiConstants {
     public static final String API_URL = "https://api.qwant.com/api/search/images?count=1&q=";
     public static final String QUERY_STATIC_PARAMS = "&t=images&safesearch=1&locale=en_US&uiv=4";
-    public static final String GOOGLE_CUSTOM_SEARCH_API_KEY = "AIzaSyAv06Dw1d6X9IV4Yus-jePkFsnOX-iSBVQ";
 }
